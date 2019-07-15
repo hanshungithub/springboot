11852d077d12fa9b30d09033c7fe1ea23954739f
hanshungithub <hshanshun@163.com>
2019-07-15 10:42:57 +0800

    condition

--- src/main/java/cn/hassan/springboot/config/TestCondition.java
+++ src/main/java/cn/hassan/springboot/config/TestCondition.java
@@ -3,13 +3,33 @@ package cn.hassan.springboot.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.MultiValueMap;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Slf4j
 public class TestCondition implements Condition {
+
+	@SuppressWarnings("unchecked")
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-        log.error(context.getClass().getCanonicalName());
-        return true;
+		boolean annotated = metadata.isAnnotated(Conditional.class.getName());
+		if (annotated) {
+			MultiValueMap<String, Object> attributes = metadata.getAllAnnotationAttributes(Conditional.class.getName(), true);
+			Object values = (attributes != null ? attributes.get("value") : null);
+			List<String[]> list = (List<String[]>) (values != null ? values : Collections.emptyList());
+			for (String[] strings : list) {
+				for (String string : strings) {
+					if (string.equalsIgnoreCase(getClass().getName())) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
     }
 }
